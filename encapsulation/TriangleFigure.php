88984ef3589b57cffb1e9d7c5d79c8e375603df2
kaneda <tkaneda@interlink.ne.jp>
2017-01-31 16:58:46 +0900

    phpstorm自動インデント

--- encapsulation/TriangleFigure.php
+++ encapsulation/TriangleFigure.php
@@ -1,35 +1,42 @@
 <?php
 
-class TriangleFigure {
+class TriangleFigure
+{
     private $base;
     private $height;
-    
-    public function __construct() {
-	$this->base   = 1;
-	$this->height = 1;
+
+    public function __construct()
+    {
+        $this->base = 1;
+        $this->height = 1;
     }
-    
-    public function getBase() {
-	return $this->base;
+
+    public function getBase()
+    {
+        return $this->base;
     }
-    
-    public function setBase($base) {
-	if (is_numeric($base) && $base > 0) {
-	    $this->base = $base;
-	}
+
+    public function setBase($base)
+    {
+        if (is_numeric($base) && $base > 0) {
+            $this->base = $base;
+        }
     }
-    
-    public function getHeight() {
-	return $this->height;
+
+    public function getHeight()
+    {
+        return $this->height;
     }
-    
-    public function setHeight($height) {
-	if (is_numeric($height) && $height > 0) {
-	    $this->height = $height;
-	}
+
+    public function setHeight($height)
+    {
+        if (is_numeric($height) && $height > 0) {
+            $this->height = $height;
+        }
     }
-    
-    public function getArea() {
-	return $this->getBase() * $this->getArea() / 2;
+
+    public function getArea()
+    {
+        return $this->getBase() * $this->getArea() / 2;
     }
 }
